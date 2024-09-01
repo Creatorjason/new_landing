a244042a5cd4156c2de8894aa64918d8e20f7583
Charles Emmanuel <relate2hazel@gmail.com>
2024-09-01 15:57:01 +0100

    chore: fixed input placing

--- components/inchatmodal/AmountInput.jsx
+++ components/inchatmodal/AmountInput.jsx
@@ -22,7 +22,7 @@ const AmountInput = ({ amount, formattedAmount, setAmount, setFormattedAmount })
         <span className="text-[34px] md:text-4xl font-bold text-[#FF8A65]">₦{formattedAmount || '0.00'}</span>
         <p className="text-sm text-gray-500 dark:text-gray-400 mt-2">Wallet balance: ₦20,000</p>
       </div>
-      <div className="flex flex-wrap items-center justify-center gap-4 mb-6 flex-grow">
+      <div className="flex flex-wrap items-center justify-center gap-4 mb-6 mx-10 flex-grow">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
           <button
             key={num}
